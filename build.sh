f24c6009e8b82915332b8a153b0e5cff2c78330c
Ruben Sanchez <rubensanchezgarcia@gmail.com>
2020-05-14 11:26:30 +0200

    Remove build scripts

--- build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
- docker build -t ruben/movieposter:0.1 . 
- 
--- run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker run -it -p 8080:8080 -e OMDB_URL=http://www.omdbapi.com -e OMDB_KEY=334b48a9 ruben/movieposter:0.1
